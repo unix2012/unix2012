de67c8099793b17ce2b0447e83acb00297f3a706
unix2012 <unix2012@126.com>
2017-09-25 22:29:38 +0800

    增加文章且改了一个栏目。

---
+++ swift/html/js/wiki.js
@@ -0,0 +1,17 @@
+$(function(){
+    // 替换外网连接
+    $("a[href^='http://']").each(function(){
+      this.target = "_blank";
+    });
+
+    $("a[href^='https://']").each(function(){
+      this.target = "_blank";
+    });
+
+        $("a[href^='ftp://']").each(function(){
+      this.target = "_blank";
+    });
+    // 替换表格样式
+   $("table").addClass("table table-bordered table-striped table-hover");
+});
+
